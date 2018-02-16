5d2b1574a986ccec2ddc58629cdbdc38bcb257b9
Kolya Mihno <klimmihno5@gmail.com>
2018-02-16 03:43:14 +0200

    Init Graph

---
+++ RainPas.pas
@@ -0,0 +1,19 @@
+program RainPas;
+
+uses graph;
+
+CONST
+
+TYPE
+
+VAR
+        gd, gm : integer;
+BEGIN
+        gd := detect;
+        gm := 0;
+        InitGraph(gd, gm, '');
+        if GraphResult <> grok then
+                halt;
+        CloseGraph;
+END.
+
